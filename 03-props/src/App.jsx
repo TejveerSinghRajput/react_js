9a2d41e55bf0301de4ead1b32db4595b8c1889ee
Tejveer Singh <tejveersingh1993@gmail.com>
2026-01-27 01:20:24 +0530

    one job card project completed

---
+++ 03-props/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './component/Card'
+
+const App = () => {
+  return (
+    <div className='parent'>
+       <div> <Card /> </div>
+        <div><Card /></div>
+    </div>
+  )
+}
+
+export default App
---
+++ 04-cards-project/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from './components/Card.jsx'
+
+const App = () =>{
+  return(
+    <div className='parent'>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+
+    // second flex
+    
+  )
+}
+export default App
