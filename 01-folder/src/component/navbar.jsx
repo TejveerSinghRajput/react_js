de16ea9c22cc8945ac80f711d63f44b55273a8d8
Tejveer Singh <tejveersingh1993@gmail.com>
2026-01-27 01:04:22 +0530

    01-folder code completed

---
+++ 01-folder/src/component/navbar.jsx
@@ -0,0 +1,13 @@
+var Navbar = () => {
+    return (
+        <div>
+        <div>Navbar</div>
+
+        <div className="nav">
+            This is nav with css changes.
+        </div>
+        </div>
+    )
+}
+
+export default Navbar
